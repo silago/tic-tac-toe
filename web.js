0a9cd1256b38c97b8733198f504fa0b671d618a9
aovchinnikov <aovchinnikov@2reallife.com>
2017-01-16 18:02:15 +0300

    without pug

--- web.js
+++ web.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
 

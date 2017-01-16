d6e337ef8f28badab0c6cd7a5b63dbd63bdfd07c
aovchinnikov <aovchinnikov@2reallife.com>
2017-01-16 18:28:42 +0300

    package update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Boot from './states/boot';
 import Game from './states/game';
-import Menu from './states/menu';
-import Preloader from './states/preloader';
-import Gameover from './states/gameover';
 
 
 const game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'tic-tac-toe-game');
